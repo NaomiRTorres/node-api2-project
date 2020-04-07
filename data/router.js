3332a7197d1c0d0581cc3ef5ddfacc298606b647
Naomi Ruemmely Torres <59476335+NaomiPriest97@users.noreply.github.com>
2020-04-07 16:52:18 -0400

    saving my finished working code

--- data/router.js
+++ data/router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const Posts = require('./db.js');
 
-// ALL OF MY GETS
+// ALL OF MY GET REQUESTS
 router.get('/', (req, res) => {
     Posts.find(req.query)
     .then(posts => {
@@ -47,7 +47,7 @@ router.get('/:id/comments', (req, res) => {
 });
 
 
-// ALL OF MY POSTS
+// ALL OF MY POST REQUESTS
 router.post('/', (req, res) => {
     Posts.insert(req.body)
     .then(post => {
@@ -68,6 +68,52 @@ router.post('/:id/comments', (req, res) => {
         res.status(500).json({ errorMessage: "error adding comment" });
     });
 });
+
+
+// DELETE REQUEST
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    Posts.remove(id)
+    .then((count) => {
+        if(count) {
+            res.status(200).json({ message: 'successfully nuked your post'});
+        } else {
+            res.status(404).json({ message: 'post not found' });
+        };
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: 'something failed, sorry'});
+    });
+});
+
+
+// PUT REQUEST  
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    console.log('changes:', changes);
+    Posts.update(req.params.id, changes)
+    .then((count) => {
+        if (count) {
+            Posts.findById(req.params.id)
+            .then((post) => {
+                res.status(200).json(post);
+            })
+            .catch((err) => {
+                res.status(500).json({ errorMessage: 'error reading the post' });
+            });
+        } else {
+            res.status(404).json({ message: 'The post could not be found' });
+        };
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error updating the post'
+        });
+    });
+});
+
 
 
 module.exports = router;
